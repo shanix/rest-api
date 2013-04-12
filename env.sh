cfef8828aa7d971460e4b025e290d08fb7a33fe8
Nick Bargnesi <nbargnesi@selventa.com>
2013-04-12 18:44:03 -0400

    env.sh; _ENV_DEPS and _ENV_BEL*

--- env.sh
+++ env.sh
@@ -15,11 +15,18 @@ export _ENV_TOPDIR="${_ENV_TOPDIR:="$DIR"}"
 export _ENV_SCRIPTS="${_ENV_SCRIPTS:="$_ENV_TOPDIR/scripts"}"
 export _ENV_SRC="${_ENV_SRC:="$_ENV_TOPDIR/src"}"
 export _ENV_TEST="${_ENV_TEST:="$_ENV_TEST/test"}"
+export _ENV_DEPS="${_ENV_DEPS:="$_ENV_TOPDIR/deps"}"
 
 ### SETTINGS ###
 export _ENV_PORT=${_ENV_PORT:-60000}
 export _ENV_OVERRIDE="${_ENV_OVERRIDE:="$_ENV_TOPDIR/env.sh.custom"}"
-
+export _ENV_BEL_ROOT="${_ENV_BEL_ROOT:="$_ENV_TOPDIR/framework"}"
+export _ENV_BEL_CACHE="${_ENV_BEL_CACHE:="$_ENV_BEL_ROOT/cache"}"
+export _ENV_BEL_WORK="${_ENV_BEL_WORK:="$_ENV_BEL_ROOT/work"}"
+DEFAULT_DBURL="jdbc:derby:${_ENV_BEL_ROOT}/db;create=true;"
+export _ENV_BEL_DBURL="${_ENV_BEL_DBURL:="$DEFAULT_DBURL"}"
+DEFAULT_RESIDX="http://resource.belframework.org/belframework/1.0/index.xml"
+export _ENV_BEL_RESIDX="${_ENV_BEL_RESIDX:="$DEFAULT_RESIDX"}"
 
 if [ -r "${CUSTOM_ENV_SH}" ]; then
     source ${CUSTOM_ENV_SH} || exit 1
